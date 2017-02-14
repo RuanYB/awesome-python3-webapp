8514e1a216d36636035479451ebd3a18254febbb
ryan <wufazhuceryan@yeah.net>
2017-02-14 19:52:51 +0800

    alter app.py and create orm.py

--- www/app.py
+++ www/app.py
@@ -1,3 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ryan ruan'
+
+
+'''
+async web application
+'''
+
 import logging; logging.basicConfig(level=logging.INFO)
 
 import asyncio, os, json, time
@@ -15,28 +25,6 @@ def init(loop):
 	srv = yield from loop.create_server(app.make_handler(), '127.0.0.1', 9000)
 	logging.info('server started at http://127.0.0.1:9000...')
 	return srv
-
-@asyncio.coroutine
-def create_pool(loop, **kw):
-	logging.info('create database connection pool')
-	global __pool
-	__pool = yield from aiomysql.create_pool(
-		host = kw.get('host', 'localhost'),
-		port = kw.get('port', 3306),
-		user = kw['user'],
-		password = kw['password'],
-		db = kw['db'],
-		charset = kw.get('charset', 'utf8'),
-		autocommit = kw.get('autocommit', True),
-		maxsize = kw.get('maxsize', 10),
-		minsize = kw.get('minsize', 1),
-		loop = loop
-		)
-
-@asyncio.coroutine
-def select(sql, args, size=None):
-	
-	
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(init(loop))
---
+++ www/orm.py
@@ -0,0 +1,84 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'ryan ruan'
+
+import asyncio, logging
+
+import aiomysql
+
+def log(sql, args=()):
+	logging.info('SQL: %s' % sql)
+
+@asyncio.coroutine
+def create_pool(loop, **kw):
+	logging.info('create database connection pool...')
+	global __pool
+	__pool = yield from aiomysql.create_pool(
+		host = kw.get('host', 'localhost'),
+		port = kw.get('port', 3306),
+		user = kw['user'],
+		password = kw['password'],
+		db = kw['db'],
+		charset = kw.get('charset', 'utf8'),
+		autocommit = kw.get('autocommit', True),
+		maxsize = kw.get('maxsize', 10),
+		minsize = kw.get('minsize', 1),
+		loop = loop
+		)
+
+#SELECT statement: return result set
+@asyncio.coroutine
+def select(sql, args, size=None):
+	log(sql, args)
+	global __pool
+	with (yield from __pool) as conn:
+		#create dict cursor
+		cur = yield from conn.cursor(aiomysql.DictCursor)
+		#execute sql query
+		yield from cur.execute(sql.replace('?', '%s'), args or ())
+		if size:
+			rs = yield from cur.fetchmany(size)
+		else:
+			rs = yield from cur.fetchall()
+		yield from cur.close()
+		logging.info('rows returned: %s' % len(rs))
+		return rs
+
+#INSERT, UPDATE, DELETE statement: return result count
+@asyncio.coroutine
+def execute(sql, args):
+	log(sql)
+	with (yield from __pool) as conn:
+		try:
+			cur = yield from conn.cursor()
+			yield from cur.execute(sql.replace('?', '%s'), args)
+			affected = cur.rowcount
+			yield from cur.close()
+		except BaseException as e:
+			raise
+		return affected
+
+#Define Model
+class Model(dict, metaclass=ModelMetaClass):
+
+	def __init__(self, **kw):
+		super(Model, self).__init__(self, **kw)
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Model' object has no attribute '%s'" % key)
+
+	def __setattr__(self, key, value):
+		self[key] = value
+
+	def getValue(self, key):
+		return getattr(self, key, None)
+
+	def getValueOrDefault(self, key):
+		value = getattr(self, key, None)
+		if value is None:
+			field = self.__mappings__[key]
+			
